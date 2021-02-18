a556a899bf84aa4ff70fdc6396cf1c2a8f2462e4
John Kao <whighwall@gmail.com>
2021-02-18 11:07:58 +0800

    fix main

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == '__main__':
     from uvicorn import run
-    run('app:app', host='0.0.0.0', port=5000, workers=2)
+    run('main:app', host='0.0.0.0', port=5000, workers=2)
