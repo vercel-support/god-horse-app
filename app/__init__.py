b99fb5a571ad191da7e96ef00e66cc28e1ebfd70
John Kao <whighwall@gmail.com>
2021-02-20 01:44:30 +0800

    app add hadle exception

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.exceptions import HTTPException, RequestValidationError
+from fastapi.exception_handlers import http_exception_handler, request_validation_exception_handler
 
 from .routers import email, ticket, image
 from .config import get_settings
@@ -7,6 +9,9 @@ from .config import get_settings
 def create_app():
     settings = get_settings()
     app = FastAPI()
+    app.add_exception_handler(HTTPException, http_exception_handler)
+    app.add_exception_handler(RequestValidationError,
+                              request_validation_exception_handler)
 
     for router in [email, ticket, image]:
         router.config(app, settings)
