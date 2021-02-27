2f421d6a4769b758059859b88dada1f056484615
John Kao <whighwall@gmail.com>
2021-02-28 00:16:51 +0800

    send_all_mail with img_id

--- app/routers/email.py
+++ app/routers/email.py
@@ -1,11 +1,13 @@
+from app.utils.image import get_img_id, update_drive_img_dirs
 import logging
 from starlette.types import Message
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, exceptions
 from fastapi import BackgroundTasks, HTTPException
 from fastapi.logger import logger
 from starlette.status import HTTP_202_ACCEPTED
 
-from ..schemas.email import EmailSchema, EmailAttachFileSchema
+from .image import drive_img_dirs
+from ..schemas.email import EmailAttachFileSchema
 from ..utils.security import api_key_checker
 from ..utils.ticket import get_tickets, updae_is_sent_list
 from ..utils.email import send_mail
@@ -28,22 +30,27 @@ async def go_send_email(message: EmailAttachFileSchema):
 @router.post("/send_all_emails", status_code=HTTP_202_ACCEPTED, deprecated=False)
 async def go_send_all_email(dir_name: str, background_tasks: BackgroundTasks):
     tickets = get_tickets(sheet_name=dir_name)
+    update_drive_img_dirs(drive_img_dirs)
     send_list = list()
     for number, title, words, name, email, is_sent in tickets[dir_name]:
         if email and name and is_sent == '0':
-            message = dict(
-                to_email=email,
-                from_email=settings.FROM_EMAIL,
-                subject='神駒團愛你',
-                html_content=f'<strong>{words} - {name}</strong>',
-                file_type='image/png',
-                file_name=settings.IMG_DIR.joinpath(
-                    f'{dir_name}/{name}.png'),
-            )
-            succeed = await send_mail(message)
-            if succeed:
-                send_list.append(number)
-            else:
+            try:
+                img_id = get_img_id(dir_name, f'{name}.png', drive_img_dirs)
+                message = dict(
+                    to_email=email,
+                    from_email=settings.FROM_EMAIL,
+                    subject='神駒團愛你',
+                    html_content=f'<strong>{words} - {name}</strong>',
+                    file_type='image/png',
+                    file_id=img_id,
+                    file_name=f'{name}.png',
+                )
+                succeed = await send_mail(message)
+                if succeed:
+                    send_list.append(number)
+                else:
+                    logger.warning(f'Fail to send the mail ({email})!!')
+            except Exception as e:
                 logger.warning(f'Fail to send the mail ({email})!!')
     logger.info('Finish sending all mails!!')
     logger.info(f'Updating the column "is_sent" of sheet ({dir_name})')
