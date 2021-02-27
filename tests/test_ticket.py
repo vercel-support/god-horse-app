dd37b9e1d5d25e7d944f5cfe01c94414f24740aa
John Kao <whighwall@gmail.com>
2021-02-28 00:21:29 +0800

    test.py update

--- tests/test_ticket.py
+++ tests/test_ticket.py
@@ -1,5 +1,6 @@
 import json
 import time
+import pytest
 
 from starlette.testclient import TestClient
 
@@ -20,11 +21,18 @@ def is_number(s):
         return False
 
 
+@pytest.mark.flaky(reruns=3, reruns_delay=3)
 def test_refresh_tickets():
     response = client.get(
-        f'{pre_fix}/refresh_tickets?sheet_name=20210227',  headers={'x-api-key': settins.API_KEY.get_secret_value()})
+        f'{pre_fix}/refresh?sheet_name=20210227',  headers={'x-api-key': settins.API_KEY.get_secret_value()})
     assert response.status_code == 200
     assert tickets and tickets.get('20210227')
+
+
+def test_get_img():
+    response = client.post(
+        f'{pre_fix}/img/20210227/template.png')
+    assert response.status_code == 200
 
 
 def test_get_ticket():
@@ -32,6 +40,7 @@ def test_get_ticket():
     response = client.get(
         f'{pre_fix}/get_ticket?sheet_name=20210227&name=test&email=test@gmail.com',  headers={'x-api-key': settins.API_KEY.get_secret_value()})
     assert response.status_code == 200
-    assert is_number(response.json().get('number'))
+    # assert is_number(response.json().get('number'))
+    assert len(response.json().get('words')) > 3
     time.sleep(0.5)
     assert n_before - len(tickets['20210227']) == 1
